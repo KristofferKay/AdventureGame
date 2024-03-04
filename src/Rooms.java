f3796730b5ee3b7cf1e53ddb5ba8f750c919a4af
ViAarisKea <viaa0001@stud.kea.dk>
2024-03-04 12:10:54 +0100

    add userInterface

---
+++ src/Rooms.java
@@ -0,0 +1,12 @@
+public class Rooms {
+String look;
+
+    public void currentRoom(String look){
+    this.look = look;
+
+    if(look.contains("look")){
+        System.out.println("looking around");
+
+    }
+}
+}
--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,2 +1,30 @@
+import java.util.Scanner;
+
 public class UserInterface {
+
+    public void navigate() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+
+           switch(input){
+               case "go north":
+                   System.out.println("Go north");
+                   break;
+               case "go south":
+                   System.out.println("Go south");
+                   break;
+               case "go west":
+                   System.out.println("Go west");
+                   break;
+               case "go east":
+                   System.out.println("Go east");
+                   break;
+           }
+
+
+
+    }
 }
