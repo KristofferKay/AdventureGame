23c193000252a3006fb62932bada97c61e85202c
samiahamzi <saah0001@stud.kea.dk>
2024-03-04 11:35:37 +0100

    currentroom

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class UserInterface {
+    Scanner input = new Scanner(System.in);
+    Adventure adventure = new Adventure();
+
+
+
+    public void startProgram() {
+        // "skriv look"
+        String noget = input.next();
+        adventure.currentRoom(noget);
+
+    }
 }
