9a9b468922f16c9bc8f2f8804b1d189816864eeb
KrisK <37834268+KristofferKay@users.noreply.github.com>
2024-03-04 16:15:54 +0100

    Userinterface changes

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -9,7 +9,7 @@ public class UserInterface {
     public void startProgram() {
         // "skriv look"
         String noget = input.next();
-        adventure.currentRoom(noget);
+//        adventure.currentRoom(noget);
 
     }
 
@@ -17,23 +17,29 @@ public class UserInterface {
 
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Welcome to the adventure game");
+        System.out.println("Please type one of the following: Go north, Go South, Go West, Go east");
+        System.out.println("You can always use Exit, help and look"); //TODO lav i et menu format
         String input = scanner.nextLine();
 
+        if (input.equals("look")) {
+            System.out.println("Looking around..");
+        }
 
-           switch(input){
-               case "go north":
-                   System.out.println("Go north");
-                   break;
-               case "go south":
-                   System.out.println("Go south");
-                   break;
-               case "go west":
-                   System.out.println("Go west");
-                   break;
-               case "go east":
-                   System.out.println("Go east");
-                   break;
-           }
+        switch(input){
+            case "go north":
+                System.out.println("Go north");
+                break;
+            case "go south":
+                System.out.println("Go south");
+                break;
+            case "go west":
+                System.out.println("Go west");
+                break;
+            case "go east":
+                System.out.println("Go east");
+                break;
+        }
 
 
 
