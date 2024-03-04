78bac6bbed2ab487b716baeba85210d7ca3d992b
ViAarisKea <viaa0001@stud.kea.dk>
2024-03-04 12:17:25 +0100

    change Room class - add attributes, getters and setters

---
+++ src/Room.java
@@ -0,0 +1,57 @@
+public class Room {
+
+    private String name;
+    private String description;
+    private Room north;
+    private Room east;
+    private Room west;
+    private Room south;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Room getNorth() {
+        return north;
+    }
+
+    public void setNorth(Room north) {
+        this.north = north;
+    }
+
+    public Room getEast() {
+        return east;
+    }
+
+    public void setEast(Room east) {
+        this.east = east;
+    }
+
+    public Room getWest() {
+        return west;
+    }
+
+    public void setWest(Room west) {
+        this.west = west;
+    }
+
+    public Room getSouth() {
+        return south;
+    }
+
+    public void setSouth(Room south) {
+        this.south = south;
+    }
+}
