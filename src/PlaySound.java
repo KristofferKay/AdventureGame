5a5e584b3168f52db49707e838c6c0b6276bca58
KrisK <37834268+KristofferKay@users.noreply.github.com>
2024-03-07 08:25:00 +0100

    Add PlaySound.java Added PlaySoundMethod in UserInterface.java. Added method call to PlaysoundMethod in Intro Added introsound.wav file in a new rsc dir.

---
+++ src/PlaySound.java
@@ -0,0 +1,27 @@
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.net.URL;
+
+public class PlaySound {
+
+    public static void afspilLydfil(String lydfilSti) {
+        try {
+            URL lydURL = PlaySound.class.getResource(lydfilSti);
+            if (lydURL == null) {
+                System.out.println("Filen blev ikke fundet: " + lydfilSti);
+                return;
+            }
+
+            AudioInputStream lydIndputStrøm = AudioSystem.getAudioInputStream(lydURL);
+
+            Clip lydClip = AudioSystem.getClip();
+            lydClip.open(lydIndputStrøm);
+
+            lydClip.start();
+            Thread.sleep(lydClip.getMicrosecondLength() / 1000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
