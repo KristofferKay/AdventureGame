1caf18afc49b2633c31257912eb472f566788f89
KrisK <37834268+KristofferKay@users.noreply.github.com>
2024-03-14 14:03:40 +0100

    Added Consumables.java and Liquid.java

---
+++ src/Consumables.java
@@ -0,0 +1,6 @@
+public class Consumables extends Item {
+    public Consumables(String shortName, String longName) {
+        super(shortName, longName);
+    }
+
+}
---
+++ src/Liquid.java
@@ -0,0 +1,5 @@
+public class Liquid extends Consumables{
+    public Liquid(String shortName, String longName) {
+        super(shortName, longName);
+    }
+}
